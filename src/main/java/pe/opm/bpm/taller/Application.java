14f019d12ebc2ca5d83e4b35d9698ceb615f20eb
Pando Morales Oscar Gregory <opandom@sunat.gob.pe>
2016-03-04 13:53:18 -0500

    modifico readme

--- src/main/java/pe/opm/bpm/taller/Application.java
+++ src/main/java/pe/opm/bpm/taller/Application.java
@@ -1,14 +1,24 @@
 package pe.opm.bpm.taller;
 
 import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import static com.google.common.base.Predicates.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.http.ResponseEntity;
 
 import com.google.common.base.Predicate;
+
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -23,8 +33,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan("pe.opm.bpm.taller")
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+    public static void main(String[] args) throws UnknownHostException {
+        //SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+        //addDefaultProfile(app, source);
+        Environment env = app.run(args).getEnvironment();
+        log.info("Access URLs:\n----------------------------------------------------------\n\t" +
+                "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                "External: \thttp://{}:{}/demo-iib/swagger-ui.html\n----------------------------------------------------------",
+            env.getProperty("server.port"),
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"));
     }
     
     @Bean
